cbd70351dbc75a66c93525d1af350d459103fb8f
zhaozhenghao1993 <736720794@qq.com>
2019-06-08 16:10:51 +0800

    fix request cookie null exception

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-![logo](doc/logo.png)
-# zh-admin
+<p align="center">
+  <a href="#">
+    <img src="https://raw.githubusercontent.com/zhaozhenghao1993/zh-web-vue/master/public/logo.png">
+  </a>
+</p>
+
+<h1 align="center">zh-admin</h1>
+
+<div align="center">
+
 [![Build Status](https://www.travis-ci.org/zhaozhenghao1993/zh-admin.svg?branch=master)](https://www.travis-ci.org/zhaozhenghao1993/zh-admin)
+
+一个简单的 springboot 权限管理 api 脚手架.
--- zh-auth/src/main/java/net/zhenghao/zh/auth/filter/ApiAuthFilter.java
+++ zh-auth/src/main/java/net/zhenghao/zh/auth/filter/ApiAuthFilter.java
@@ -154,9 +154,11 @@ public class ApiAuthFilter implements Filter {
         }
         if (StringUtils.isBlank(authToken)) {
             Cookie[] cookies = request.getCookies();
-            for (Cookie cookie: cookies) {
-                if (cookie.getName().equals(tokenHeaderConfig.getTokenHeader())) {
-                    authToken = cookie.getValue();
+            if (cookies != null) {
+                for (Cookie cookie: cookies) {
+                    if (cookie.getName().equals(tokenHeaderConfig.getTokenHeader())) {
+                        authToken = cookie.getValue();
+                    }
                 }
             }
         }
