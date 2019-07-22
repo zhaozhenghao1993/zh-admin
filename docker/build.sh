5cd846143eed63603dd13caee8b1a1384154ab8a
zhaozhenghao1993 <736720794@qq.com>
2019-07-22 21:23:22 +0800

    add build.sh & add druid login filter

---
+++ docker/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "build starting ..."
+cd ..
+echo "mvn package starting ..."
+mvn clean
+mvn package -DskipTests=true -Dmaven.javadoc.skip=true
+echo "mvn package finished ..."
+
+cd zh-main
+docker build -t zhaozhenghao1993/zh-admin:1.0.0 .
+echo "build finished ..."
