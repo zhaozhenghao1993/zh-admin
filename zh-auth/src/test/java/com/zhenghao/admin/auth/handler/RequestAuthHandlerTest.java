a9267ff178b81614c6c039a547075d3eeff7180a
zhaozhenghao1993 <736720794@qq.com>
2019-07-25 23:29:20 +0800

    junit test

---
+++ zh-auth/src/test/java/com/zhenghao/admin/auth/handler/RequestAuthHandlerTest.java
@@ -0,0 +1,64 @@
+package com.zhenghao.admin.auth.handler;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RequestAuthHandler.class)
+public class RequestAuthHandlerTest {
+
+    @Autowired
+    private RequestAuthHandler requestAuthHandler;
+
+    @Test
+    public void validateAnnoFilterChain() {
+        Assert.assertTrue("可以直接匿名访问uri",
+                requestAuthHandler.validateAnnoFilterChain("/sys/login", "GET"));
+    }
+
+    @Test
+    public void validateAuthFilterChain() {
+        Assert.assertTrue("需要登陆才能访问uri",
+                requestAuthHandler.validateAuthFilterChain("/sys/user/info", "GET"));
+    }
+
+    @Test
+    public void validatePermsFilterChain() {
+        Assert.assertTrue("需要授权才能访问uri",
+                requestAuthHandler.validateAuthFilterChain("/sys/user", "POST"));
+    }
+
+    @Test
+    public void validate() {
+        Assert.assertTrue(
+                "验证restful风格uri",
+                RequestAuthHandler.validate(
+                        "/sys/user/1/reset",
+                        "PUT",
+                        "/sys/user/{id}/reset",
+                        "PUT")
+        );
+
+        Assert.assertTrue(
+                "验证通配符风格uri",
+                RequestAuthHandler.validate(
+                        "/druid/wall.html",
+                        "GET",
+                        "/druid/*",
+                        "GET")
+        );
+
+        Assert.assertTrue(
+                "综合验证uri",
+                RequestAuthHandler.validate(
+                        "/uploads/-/system/user/avatar/1/avatar.jpg",
+                        "GET",
+                        "/uploads/-/system/user/avatar/{id}/*",
+                        "GET")
+        );
+    }
+}
