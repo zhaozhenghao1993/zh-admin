3f957b4991ec059a7f36a4ab597543a387dfa672
zhaozhenghao1993 <736720794@qq.com>
2019-07-27 19:26:51 +0800

    update docker

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 echo "starting ..."
-docker run -e PARAMS="--spring.datasource.druid.url=jdbc:mysql://192.168.0.104:3306/test-admin? \
-useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&serverTimezone=GMT%2B8 \
---spring.datasource.druid.username=root --spring.datasource.druid.username=password" \
--p 8081:8081 -v /tmp/app/zh-admin:/data/app/zh-admin -v /zh-main/application.yaml:/app/application.yaml \
---name zh-admin -d zhaozhenghao1993/zh-admin:1.0.0
+docker run -e PARAMS="--spring.datasource.druid.url=jdbc:mysql://192.168.0.104:3306/test-admin?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&serverTimezone=GMT%2B8 --spring.datasource.druid.username=root --spring.datasource.druid.password=root --zh-admin.file.log.folder=/data/app/zh-admin/log" -p 8081:8081 -v /tmp:/data/app --name zh-admin -d zhaozhenghao1993/zh-admin:1.0.0
 echo "finished ..."
