543aec0c8094f85a17c779d4373d5cd1c05511b6
zhaozhenghao1993 <736720794@qq.com>
2019-08-13 19:46:41 +0800

    test the IP fetch inside the container

--- zh-auth/src/main/java/com/zhenghao/admin/auth/config/FilterChainConfig.java
+++ zh-auth/src/main/java/com/zhenghao/admin/auth/config/FilterChainConfig.java
@@ -34,7 +34,7 @@ public class FilterChainConfig {
     public void init() {
         annoFilterChainList.add(new SysPermissionEntity("/sys/login", "POST"));
         annoFilterChainList.add(new SysPermissionEntity("/test/heart", "GET"));
-        annoFilterChainList.add(new SysPermissionEntity("/test/test", "GET"));
+        annoFilterChainList.add(new SysPermissionEntity("/test/ip", "GET"));
 
         authFilterChainList.add(new SysPermissionEntity("/sys/user/info", "GET"));
         authFilterChainList.add(new SysPermissionEntity("/sys/user/profile", "PUT"));
